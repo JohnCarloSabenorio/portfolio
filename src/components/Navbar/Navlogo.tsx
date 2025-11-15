c8aebfd8d0bc849062e7c074d5168acd2f8afcc1
Bluem <140798194+JohnCarloSabenorio@users.noreply.github.com>
2025-11-16 00:29:01 +0800

    edited nav

--- src/components/Navbar/Navlogo.tsx
+++ src/components/Navbar/Navlogo.tsx
@@ -1,12 +1,7 @@
 export default function Navlogo() {
   return (
     <div className="nav-flex">
-      <img
-        src="images/jc-logo.png"
-        className="image-size bg-black rounded-full"
-        alt="JC's Logo"
-      />
-      <h1 className="text-2xl font-semibold">My Portfolio</h1>
+      <h1 className="text-3xl font-bold text-black">ᜇᜌ ᜐᜒ</h1>
     </div>
   );
 }
