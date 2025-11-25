ee91025436fe04c927f396590c1fe5af76166636
Bluem <140798194+JohnCarloSabenorio@users.noreply.github.com>
2025-11-25 19:22:01 +0800

    commit

--- src/components/Projects/ProjectItem.tsx
+++ src/components/Projects/ProjectItem.tsx
@@ -21,7 +21,7 @@ export default function ProjectItem({ projectData }: { projectData: Project }) {
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 -960 960 960"
-            className="cursor-pointer w-5 aspect-square hover:w-7 transition-all"
+            className="cursor-pointer w-5 aspect-square hover:scale-150 transition-all"
           >
             <path d="m256-240-56-56 384-384H240v-80h480v480h-80v-344L256-240Z" />
           </svg>
